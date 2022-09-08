2b8873eef778bbb34833ac19a2fc926a703a3524
Viacheslav Kovalenko <kovalenkova1983@gmail.com>
2022-09-08 22:56:06 +0300

    add 5.2

---
+++ js/homework_5_2.js
@@ -0,0 +1,39 @@
+﻿function People(name, age) 
+{
+        this.name = name;
+        this.age = age;
+}
+
+var people = new People();
+var peoples = [];
+peoples.push(new People('Vasa', 19));
+peoples.push(new People('Peta', 20));
+peoples.push(new People('Anna', 24));
+peoples.push(new People('Towrik', 26));
+peoples.push(new People('Rey', 18));
+                
+
+
+function buttonup() {
+peoples.sort(function(a, b)
+{
+        return a.age-b.age
+});
+
+for (var i = 0; i < peoples.length;i++) 
+{
+document.write(peoples[i].name + " | "  + " | " + peoples[i].age + "<br />");
+};
+}
+
+function buttondown() {
+        peoples.sort(function(a, b)
+        {
+                return b.age-a.age
+        });
+        
+        for (var i = 0; i < peoples.length;i++) 
+        {
+        document.write(peoples[i].name + " | "  + " | " + peoples[i].age + "<br />");
+        };
+        }
