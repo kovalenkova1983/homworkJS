01960fc8112b3968a9dc4c19e34ba90da989accf
Viacheslav Kovalenko <kovalenkova1983@gmail.com>
2022-09-11 20:56:56 +0300

    add 1.2 and correct 4.2

--- js/homework_4_1.js
+++ js/homework_4_1.js
@@ -2,13 +2,14 @@
 Напиши функцию map(fn, array), которая принимает на вход функцию и массив,
  и обрабатывает каждый элемент массива этой функцией, возвращая новый массив. */
 
- 
-function map(fn, array) 
-{
+
+function map(fn, array) {
     const newArray = []
-    for (let i = 0; i < array.length; ++i)
-     {
+    for (let i = 0; i < array.length; ++i) {
         newArray.push(fn(array[i]));
     }
-        return newArray
- }
+    return newArray
+}
+function addition(x) { return x + x; }
+console.log(map(addition, [1, 2, 3, 4]));//2,4,6,8
+console.log(map(addition, []));//[]
--- js/homework_4_2.js
+++ js/homework_4_2.js
@@ -8,21 +8,38 @@
 5 return confirm('Родители разрешили?');
 6 } } */
 
-function button1()
-{
-    document.getElementById("monitor").innerHTML = "function checkAge(age) {<br/>return (age > 18) ? true : confirm('Родители разрешили?');<br\n>}";
+function button1() {
+  document.getElementById("monitor").innerHTML = "function checkAge(age) {<br/>return (age > 18) ? true : confirm('Родители разрешили?');<br\n>}";
 
-    function checkAge(age)
-     {
-        return (age > 18) ? true : confirm('Родители разрешили?');
-      }
+  function checkAge(age) {
+    return (age > 18) ? true : confirm('Родители разрешили?');
+  }
 }
 
-function button2()
-{
-    document.getElementById("monitor").innerHTML = "function checkAge(age) {<br/>return (age > 18) || confirm('Родители разрешили?');<br\n>}";
-    function checkAge(age)
-    {
-        return (age > 18) || confirm('Родители разрешили?');
-      }
+function button2() {
+  document.getElementById("monitor").innerHTML = "function checkAge(age) {<br/>return (age > 18) || confirm('Родители разрешили?');<br\n>}";
+  function checkAge(age) {
+    return (age > 18) || confirm('Родители разрешили?');
+  }
+}
+
+function button3() {
+  const ageDate = prompt("Введите ваш возраст: ");
+checkAge(ageDate);
+  
+
+function checkAge(age) {
+    return (ageDate > 18) ? alert("Можем не спрашивать предков!!") : qestAge();
+
+    
+    
+    function qestAge() {
+      let qest = confirm('Родители разрешили?');
+      if (qest) { alert("Родители разрешили!!!"); }
+      else { alert("Родители запретили!!!"); }
+    }
+
+
+  }
+
 }
